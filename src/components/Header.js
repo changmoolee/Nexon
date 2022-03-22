d77b0b3fec7c6c80c6ea11594511685b3083a890
Lee Changmoo <lcm8543@gmail.com>
2022-03-22 17:01:20 +0900

    Add: Header UI 완성

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,45 +1,112 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import Nav from "./Header/Nav";
+import Drawer from "./Header/Drawer";
 
-const Outer = styled.div`
+const Outer = styled.section`
   width: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
   border: black 1px solid;
 `;
-const Inner = styled.div`
-  width: 70%;
-  display: flex;
-  border: red 1px solid;
-`;
 const Util = styled.div`
   width: 100%;
   height: 48px;
   display: flex;
+  justify-content: center;
   align-items: center;
 `;
-const Logo = styled.img`
-  width: 90px;
-  object-fit: contain;
+const Inner = styled.div`
+  width: 70%;
+  height: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 `;
-const Nav = styled.div`
+const LogoContainer = styled.div`
+  height: 48px;
+  display: flex;
+  align-items: center;
+  color: #ccc;
+`;
+const Logo = styled.img`
+  object-fit: contain;
+  padding-left: 8px;
+  box-sizing: border-box;
+`;
+const KartRiderLogo = styled.div`
+  height: 100%;
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  :hover {
+    background: #f2f2f2;
+  }
+`;
+const TMILogo = styled.div`
+  cursor: pointer;
+`;
+
+const Homepage = styled.span`
+  font-size: 12px;
+  font-weight: 600;
+  color: #6c7a89;
+  cursor: pointer;
+`;
+const Container = styled.section`
+  display: flex;
+  flex-direction: column;
   width: 100%;
-  height: 55px;
 `;
 
 const Header = () => {
+  const [hovered, setHovered] = useState(null);
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  // Nav의 "카트", "트랙" item을 hover할때 나타나는 Drawer의 state
+
+  const openDrawer = () => {
+    setIsDrawerOpen(true);
+  };
+  // Drawer on
+  const closeDrawer = () => {
+    setIsDrawerOpen(false);
+  };
+  // Drawer off
+
+  const onMouseLeave = () => {
+    closeDrawer();
+  };
+  // mouse가 떠났을 때 Drawer off하기
+
   return (
-    <Outer>
-      <Inner>
+    <>
+      <Outer>
         <Util>
-          <Logo src="image/logo.jpeg" />
+          <Inner>
+            <LogoContainer>
+              <KartRiderLogo>
+                <Logo src="https://tmi.nexon.com/img/assets/logo_kart.png" />
+                <ArrowDropDownIcon />
+              </KartRiderLogo>
+              <TMILogo>
+                <Logo src="https://tmi.nexon.com/img/assets/tmi_logo_default_b.svg" />
+              </TMILogo>
+            </LogoContainer>
+            <Homepage>카트라이더 홈페이지 바로가기</Homepage>
+          </Inner>
         </Util>
-      </Inner>
-      <Inner>
-        <Nav></Nav>
-      </Inner>
-    </Outer>
+        <Container onMouseLeave={onMouseLeave}>
+          <Nav
+            setHovered={setHovered}
+            openDrawer={openDrawer}
+            closeDrawer={closeDrawer}
+          />
+          {isDrawerOpen ? <Drawer hovered={hovered} /> : null}
+        </Container>
+      </Outer>
+    </>
   );
 };
 
---
+++ src/components/Header/Nav.js
@@ -0,0 +1,141 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import SearchIcon from "@mui/icons-material/Search";
+
+const Inner = styled.div`
+  width: 70%;
+  height: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Container = styled.div`
+  width: 100%;
+  height: 55px;
+  display: flex;
+  justify-content: center;
+  background-color: #005fcc;
+`;
+
+const Items = styled.section`
+  display: flex;
+`;
+const Item = styled.div`
+  width: 80px;
+  height: 55px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-right: 40px;
+  box-sizing: border-box;
+  color: #fff;
+  opacity: 0.5;
+  border-bottom: ${({ clicked, index }) =>
+    clicked === index ? "4px solid white" : null};
+  font-weight: ${({ clicked, index }) => (clicked === index ? "700" : null)};
+  opacity: ${({ clicked, index }) => (clicked === index ? "1" : "0.5")};
+  transition: ${({ clicked, index }) =>
+    clicked === index ? "all 0.15s ease-in-out" : null};
+  :hover {
+    border-bottom: 4px solid white;
+    font-weight: 700;
+    opacity: 1;
+    transition: all 0.15s ease-in-out;
+  }
+`;
+const Text = styled.a`
+  margin-bottom: ${({ clicked, index }) => (clicked === index ? "-4px" : null)};
+  ${Item}:hover & {
+    margin-bottom: -4px;
+    transition: all 0.15s ease-in-out;
+  }
+`;
+
+const SearchContainer = styled.div`
+  width: 220px;
+  height: 32px;
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid #fff;
+  color: white;
+  opacity: 0.5;
+  transition: 0.3s;
+  :hover {
+    opacity: 1;
+  }
+  :focus {
+    opacity: 1;
+  }
+`;
+
+const Search = styled.input`
+  width: 195px;
+  height: 32px;
+  padding-left: 10px;
+  padding-right: 25px;
+  font-size: 12px;
+  border: none;
+  color: white;
+  background: none;
+  opacity: 0.8;
+  font-weight: 700;
+  outline: none;
+  ::placeholder {
+    color: white;
+  }
+  :focus::placeholder {
+    color: transparent;
+  }
+`;
+
+const Nav = ({ setHovered, openDrawer, closeDrawer }) => {
+  const items = ["홈", "랭킹", "카트", "트랙"];
+  const [clicked, setClicked] = useState(null);
+  // 클릭한 아이템의 index를 저장하는 state
+
+  const onClickItem = (index) => {
+    setClicked(index);
+  };
+
+  const onMouseEnter = (index) => {
+    if (index === 2) {
+      setHovered(2);
+      openDrawer();
+    } else if (index === 3) {
+      setHovered(3);
+      openDrawer();
+    } else {
+      setHovered(null);
+      closeDrawer();
+    }
+  };
+
+  return (
+    <Container>
+      <Inner>
+        <Items>
+          {items.map((item, index) => (
+            <Item
+              key={item}
+              clicked={clicked}
+              index={index}
+              onClick={() => onClickItem(index)}
+              onMouseEnter={() => onMouseEnter(index)}
+            >
+              <Text clicked={clicked} index={index}>
+                {item}
+              </Text>
+            </Item>
+          ))}
+        </Items>
+        <SearchContainer>
+          <Search placeholder="닉네임 검색" />
+          <SearchIcon />
+        </SearchContainer>
+      </Inner>
+    </Container>
+  );
+};
+
+export default Nav;
