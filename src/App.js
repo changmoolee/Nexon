57a91646bd35cfb23ecef42bf261dcc97da73f70
Lee Changmoo <lcm8543@gmail.com>
2022-03-22 17:25:13 +0900

    Add: Footer UI 완성

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import styled from "styled-components";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 import Main from "./pages/Main";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Main />
+      <Footer />
     </div>
   );
 }
